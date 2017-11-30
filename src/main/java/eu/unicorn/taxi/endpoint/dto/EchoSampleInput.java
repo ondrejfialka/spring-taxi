840f9f04c2c764319472cad622eaf89d54ba84a5
Ondrej Fialka <ondrej.fialka@unicorncollege.cz>
2017-11-30 14:18:38 +0100

    no message

--- src/main/java/eu/unicorn/taxi/endpoint/dto/EchoSampleInput.java
+++ src/main/java/eu/unicorn/taxi/endpoint/dto/EchoSampleInput.java
@@ -1,9 +1,18 @@
 package eu.unicorn.taxi.endpoint.dto;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 public class EchoSampleInput {
 
 	private String name;
-	private Integer age;
+	
+	private Date dateTime;
 	
 	public String getName() {
 		return name;
@@ -11,10 +20,12 @@ public class EchoSampleInput {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Integer getAge() {
-		return age;
+	public Date getDateTime() {
+		return dateTime;
 	}
-	public void setAge(Integer age) {
-		this.age = age;
+	public void setDateTime(Date dateTime) {
+		this.dateTime = dateTime;
 	}
+	
+	
 }
